a50e2f48bc2c3e7b03bbaa5c6942613396eb399f
Jeongwoo-KGI <61396660+Jeongwoo-KGI@users.noreply.github.com>
2024-12-17 09:52:28 +0900

    Create 1217MC.dart

---
+++ morning codes/1217MC.dart
@@ -0,0 +1,19 @@
+
+void rotate(List<List<int>> matrix) {
+  int n = matrix.length;
+
+  // Step 1: Transpose the matrix
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      // Swap matrix[i][j] with matrix[j][i]
+      int temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  // Step 2: Reverse each row
+  for (int i = 0; i < n; i++) {
+    matrix[i] = matrix[i].reversed.toList();
+  }
+}
